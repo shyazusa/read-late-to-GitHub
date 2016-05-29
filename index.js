cfbc0fefe10f0e4ad22acce9dc13367783105f08
shyazusa <shy.azusa@gmail.com>
2016-05-29 12:04:28 +0900

    Add main function

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-var self = require("sdk/self");
-
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
-
-exports.dummy = dummy;
-
-var buttons = require('sdk/ui/button/action');
 var tabs = require('sdk/tabs');
+var github = require('./github.json');
+var Request = require("sdk/request").Request;
+var buttons = require('sdk/ui/button/action');
 
 var button = buttons.ActionButton({
-  id: 'mozilla-link',
-  label: 'Visit Mozilla',
+  id: 'create-issue-for-gitgub',
+  label: 'Create issue',
   icon: {
     '16': './icon-16.png',
     '32': './icon-32.png',
@@ -23,5 +15,23 @@ var button = buttons.ActionButton({
 });
 
 function handleClick(state) {
-  tabs.open('https://www.mozilla.org/');
+  var user = github.user;
+  var repo = github.repo;
+  var token = github.token;
+  var title = tabs.activeTab.title;
+  var body = '[' + title + ']' + '(' + tabs.activeTab.url + ')';
+  var query = JSON.stringify({
+    'title': title,
+    'body': body,
+    'labels': ['readlate', 'memo']
+  });
+  var url = 'https://api.github.com/repos/' + user +'/' + repo + '/issues?access_token=' + token;
+  Request({
+    url: url,
+    content: query,
+    onComplete: function(res) {
+      console.log(res.text);
+    }
+  }).post();
+  console.log(url);
 }
